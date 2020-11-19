afa72023068c27a23986f8de1c889132897c37d7
Thomas Yuan <thomas@cppcoder.net>
2020-11-19 11:41:13 -0500

    change color

--- game1/main.js
+++ game1/main.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 var x = 0;
-var y = 0;
+var y = 20;
 var dx = 1;
 var dy = 1;
 
@@ -14,15 +14,15 @@ function draw() {
   x = x + dx;
   y = y + dy;
 
-  if (x < 0 || x > canvas.width) {
+  if (x < 0 || x + 200 > canvas.width) {
     dx = -dx;
     ctx.fillStyle = "hsl(" + 360 * Math.random() + ", 50%, 50%)";
   }
 
-  if (y < 0 || y > canvas.height) {
+  if (y < 20 || y + 4 > canvas.height) {
     dy = -dy;
     ctx.fillStyle = "hsl(" + 360 * Math.random() + ", 50%, 50%)";
   }
 }
 
-setInterval(draw, 20);
+setInterval(draw, 10);
