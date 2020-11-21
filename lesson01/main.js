4ab76399831ead097c0da443ee0d294d296e5f0e
Thomas Yuan <thomas@cppcoder.net>
2020-11-21 15:27:50 -0500

    new code  very fun

--- lesson01/main.js
+++ lesson01/main.js
@@ -3,8 +3,8 @@ var ctx = canvas.getContext("2d");
 
 var x = 0;
 var y = 0;
-var dx = 1;
-var dy = 1;
+var xSpeed = 1;
+var ySpeed = 1;
 
 function resize() {
   ctx.canvas.width = window.innerWidth - 20;
@@ -13,20 +13,19 @@ function resize() {
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 50%, 50%)'
   ctx.font = ctx.font.replace(/\d+px/, "28px");
   ctx.fillText("Happy Halloween!", x, y, 200);
 
-  x = x + dx;
-  y = y + dy;
+  x = x + xSpeed;
+  y = y + ySpeed;
 
-  if (x + 200 > canvas.width) dx = -1;
-  if (y > canvas.height) dy = -1;
-  if (x < 0) dx = 1;
-  if (y - 28 < 0) dy = 1;
+  if (x + 200 > canvas.width) xSpeed = -1;
+  if (y > canvas.height) ySpeed = -1;
+  if (x < 0) xSpeed = 1;
+  if (y - 28 < 0) ySpeed = 1;
 }
 
 window.onresize = resize;
 
 resize();
-setInterval(draw, 20);
+setInterval(draw, 10);
