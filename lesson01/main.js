5c33ed9924a50259c96f5d01902b07e51d589ca0
Thomas Yuan <thomas@cppcoder.net>
2020-11-21 14:52:39 -0500

    resized canvas and fixed bugs

--- lesson01/main.js
+++ lesson01/main.js
@@ -6,6 +6,11 @@ var y = 0;
 var dx = 1;
 var dy = 1;
 
+function resize() {
+  ctx.canvas.width = window.innerWidth - 20;
+  ctx.canvas.height = window.innerHeight - 20;
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   // ctx.fillStyle = 'hsl(' + 360 * Math.random() + ', 50%, 50%)'
@@ -15,10 +20,13 @@ function draw() {
   x = x + dx;
   y = y + dy;
 
-  if (x > canvas.width) dx = -1;
+  if (x + 200 > canvas.width) dx = -1;
   if (y > canvas.height) dy = -1;
   if (x < 0) dx = 1;
-  if (y < 0) dy = 1;
+  if (y - 28 < 0) dy = 1;
 }
 
+window.onresize = resize;
+
+resize();
 setInterval(draw, 20);
