fb071c1361b8c4a3581d49a1da39904d76c60e63
Thomas Yuan <thomas@cppcoder.net>
2020-11-22 21:12:35 -0500

    fixed bug

--- lesson02/main.js
+++ lesson02/main.js
@@ -1,6 +1,9 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+const BALL_NUMBER = 50;
+const MAX_RADIUS = 20;
+
 class Ball {
   constructor(ctx) {
     this.ctx = ctx;
@@ -10,7 +13,7 @@ class Ball {
     this.color = "hsl(" + 360 * Math.random() + ", 50%, 50%)";
     this.xSpeed = Math.random() * 5;
     this.ySpeed = Math.random() * 5;
-    this.radius = Math.random() * 20;
+    this.radius = Math.random() * MAX_RADIUS;
   }
 
   draw() {
@@ -25,30 +28,39 @@ class Ball {
 
     // Bouncing
     // right side
-    if (this.x + this.radius > canvas.width) this.xSpeed = -this.xSpeed;
+    if (this.x + this.radius > canvas.width && this.xSpeed > 0) {
+      this.xSpeed = -this.xSpeed;
+    }
+
     // bottom
-    if (this.y + this.radius > canvas.height) this.ySpeed = -this.ySpeed;
+    if (this.y + this.radius > canvas.height && this.ySpeed > 0) {
+      this.ySpeed = -this.ySpeed;
+    }
     // left side
-    if (this.x - this.radius < 0) this.xSpeed = -this.xSpeed;
+    if (this.x - this.radius < 0 && this.xSpeed < 0) {
+      this.xSpeed = -this.xSpeed;
+    }
     // top
-    if (this.y - this.radius < 0) this.ySpeed = -this.ySpeed;
+    if (this.y - this.radius < 0 && this.ySpeed < 0) {
+      this.ySpeed = -this.ySpeed;
+    }
   }
 }
 
 var balls = [];
-for (var i = 0; i < 50; ++i) {
+for (var i = 0; i < BALL_NUMBER; ++i) {
   balls.push(new Ball(ctx));
 }
 
 function resize() {
-  ctx.canvas.width = window.innerWidth - 20;
-  ctx.canvas.height = window.innerHeight - 20;
+  ctx.canvas.width = window.innerWidth - MAX_RADIUS;
+  ctx.canvas.height = window.innerHeight - MAX_RADIUS;
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  for (var i = 0; i < 50; i++) {
+  for (var i = 0; i < BALL_NUMBER; i++) {
     balls[i].draw();
   }
 }
